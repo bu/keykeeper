b4474b211117c2167533d7949500146501fd3ebd
Buwei Chiu <bu@x2d.cc>
2024-04-07 02:08:23 +0800

    Update version message content

--- cmd/kk/handler/version.go
+++ cmd/kk/handler/version.go
@@ -3,7 +3,13 @@ package handler
 import (
 	"fmt"
 	"runtime/debug"
+	"strings"
+	"time"
 )
+
+// ReleaseVersion is the version of the application
+// TODO: update this version before release
+const ReleaseVersion = "0.1.0"
 
 func init() {
 	prepareRepo()
@@ -18,7 +24,19 @@ type VersionHandler struct{}
 func (h *VersionHandler) Handle(env *HandlerEnv) error {
 	version, versionTime := h.extractVcsInfo()
 
-	fmt.Printf("KeyKeeper %s (built at %s)\n", version, versionTime)
+	buildTime, err := time.Parse(time.RFC3339, versionTime)
+	if err != nil {
+		buildTime = time.Time{}
+	}
+
+	fmt.Printf("[KeyKeeper] v%s\nBuild #%s, built on %s\n", ReleaseVersion, version[:7], buildTime.Format("January 02, 2006"))
+	fmt.Println(strings.Repeat("-", 40))
+	fmt.Printf("Copyright (C) 2024 Buwei Chiu\n")
+	fmt.Println(strings.Repeat("-", 40))
+	fmt.Println("KeyKeeper is a password manager that helps you manage your passwords securely.")
+	fmt.Println("This software is distributed under the MIT License. See LICENSE for more information.")
+	fmt.Println("For more information, please visit https://github.com/bu/keykeeper")
+
 	return nil
 }
 
